553b1217db392b39255ec829facea2900f6e0c07
Unknown <bencyr39@gmail.com>
2017-03-23 18:36:07 -0500

    Fixed a Logic Error in Jump Logic
    Made it so TakeJump and Halt signals defaulted to '0' rather than undefined

--- src/IF_Stage/Jump_Logic.vhd
+++ src/IF_Stage/Jump_Logic.vhd
@@ -18,13 +18,13 @@ entity Jump_Logic is
 end entity Jump_Logic;
 
 architecture Jump_Logic_Behavior of Jump_Logic is
-	signal Opcode : std_logic_vector(RegWidth-1 downto RegWidth-4);
+	signal Opcode : std_logic_vector(3 downto 0);
 	signal JumpBits: std_logic_vector(RegWidth-5 downto 0);
 	begin
 		Opcode <= Instruction(RegWidth-1 downto RegWidth-4);
 		JumpBits <= Instruction(RegWidth-5 downto 0);
-		TakeJump <= '1' when Opcode = "1110";
-		Halt <= '1' when Opcode = "0000";
+		TakeJump <= '1' when Opcode = "1110" else '0';
+		Halt <= '1' when Opcode = "0000" else '0';
 		JumpAddress <= PC(RegWidth-1 downto RegWidth-4) & JumpBits;
 end architecture Jump_Logic_Behavior;
 
