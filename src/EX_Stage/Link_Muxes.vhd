2fb4d012a368ad4f444cd9e20bc265f14f2d8c21
Unknown <bencyr39@gmail.com>
2017-03-23 20:17:56 -0500

    Fixed a Couple Typing Errors
    ALU_Muxes => Link_Muxes

--- src/EX_Stage/Link_Muxes.vhd
+++ src/EX_Stage/Link_Muxes.vhd
@@ -15,11 +15,11 @@ entity Link_Muxes is
 		DestReg_In: in std_logic_vector(AddrBits-1 downto 0);
 		DestReg_Out: out std_logic_vector(AddrBits-1 downto 0);
 		ALUOut_In: in std_logic_vector(RegWidth-1 downto 0);
-		ALUOut_Out: in std_logic_vector(RegWidth-1 downto 0);
+		ALUOut_Out: out std_logic_vector(RegWidth-1 downto 0);
 		PC: in std_logic_vector(RegWidth-1 downto 0));
 end entity Link_Muxes;
 
-architecture Link_Muxes_Behavior of ALU_Muxes is
+architecture Link_Muxes_Behavior of Link_Muxes is
 	begin
 		ALUOut_Out <= PC when Link = '1' else ALUOut_In;
 		DestReg_Out <= (others => '1') when Link = '1' else DestReg_In;
