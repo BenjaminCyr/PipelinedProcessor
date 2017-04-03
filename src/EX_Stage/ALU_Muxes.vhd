d2387f8a6ff0c123abb2299ad8a7af501ca128e0
Ben Cyr <bencyr39@gmail.com>
2017-04-03 15:34:24 -0500

    Fixed Hazard by Forwarding MemData
    If Register Modified in instruction before SW, the data written to the data would have been incorrect. Fixed the hazard by adding new "Mux"

--- src/EX_Stage/ALU_Muxes.vhd
+++ src/EX_Stage/ALU_Muxes.vhd
@@ -24,17 +24,21 @@ entity ALU_Muxes is
 		ReadData2: in std_logic_vector(RegWidth-1 downto 0);
 		Imm: in std_logic_vector(RegWidth-1 downto 0);
 		Operand1: out std_logic_vector(RegWidth-1 downto 0);
-		Operand2: out std_logic_vector(RegWidth-1 downto 0));
+		Operand2: out std_logic_vector(RegWidth-1 downto 0);
+		MemData: out std_logic_vector(RegWidth-1 downto 0));
 end entity ALU_Muxes;
 
 architecture ALU_Muxes_Behavior of ALU_Muxes is
+	signal Fw_Operand : std_logic_vector(RegWidth-1 downto 0);
 	begin
 		DestReg <= Rd when RegDst = '1' else Rt;
 		Operand1 <= WB_WriteData when ForwardA = "10" else
 					Mem_ALUOut when ForwardA = "01" else
 					ReadData1;
+		Fw_Operand <= WB_WriteData when ForwardB = "10" else
+					Mem_ALUOut when ForwardA = "01" else
+					ReadData2
 		Operand2 <= Imm when ALUSrc = '1' else 
-					WB_WriteData when ForwardB = "10" else
-					Mem_ALUOut when ForwardB = "01" else
-					ReadData2;
+					Fw_Operand;
+		MemData <= Fw_Operand;
 end architecture ALU_Muxes_Behavior;
