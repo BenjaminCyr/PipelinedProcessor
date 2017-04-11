6280250fa527b9117593d86fb074480c7cda9842
Unknown <bencyr39@gmail.com>
2017-04-10 20:42:45 -0500

    Fixed Error to Show Time

--- testbenches/Top_Level/test_PipelinedProcessor.v
+++ testbenches/Top_Level/test_PipelinedProcessor.v
@@ -67,7 +67,7 @@ begin
 	#0 $display ("	ADDI $2, $0, 4\n  ADDI $3, $0, 1\nL1: SUB $2, $2, $3\n  BNE $2, $3, L1\n  ADD $4, $2, $3\n  OR $5, $4, $3\n  AND $6, $5, $3\nL2: BEQ $5, $0, L3\n  LSI $5, $5, 4\n  RSI $4, $4, 1\n  J L2\nL3: ADDI $2, $0, 5\n  JL FIB\n  SLT $2, $0, $1\n  HALT\nFIB: ADDI $1, $0, 1\n  ADDI $3, $0, 1\nL4: ADDI $2, -1\n  BEQ $2, $0, L5\n  ADD $4, $3, $1\n  ADD $1, $3, $0\n  ADD $3, $4, $0\n  J L4\nL5: ADD $1, $4, $0\n  JR $7\n");
 	#0 $display ("Time, inr, out_value,");
 	#10 RST = 0;
-	#800 $monitor ("%g ns, %x, %x,", $time, inr, out_value);
+	#800 $monitor ("%t, %x, %x,", $time, inr, out_value);
 	for (j=0; j<TotalReg; j=j+1)
 	begin
 		inr = j;
