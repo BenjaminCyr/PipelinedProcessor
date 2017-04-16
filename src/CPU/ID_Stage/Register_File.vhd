250ffbcfc8d380e1b86f79f29a374e0d4dbf62c5
Unknown <bencyr39@gmail.com>
2017-04-16 14:29:40 -0500

    Made Register File easier to Synthesize. Can Download to Nexys Board
    Successful Demo on Nexys Board, but need to fix RST

--- src/CPU/ID_Stage/Register_File.vhd
+++ src/CPU/ID_Stage/Register_File.vhd
@@ -26,29 +26,26 @@ entity Register_File is
 end entity Register_File;
 
 architecture Register_File_Behavior of Register_File is
-	constant zero_reg : std_logic_vector(AddrBits-1 downto 0) := (others => '0');
-	constant x_reg : std_logic_vector(AddrBits-1 downto 0) := (others => 'X');
-	constant u_reg : std_logic_vector(AddrBits-1 downto 0) := (others => 'U');
+	signal index1,index2,index3 : unsigned(AddrBits-1 downto 0);
 	subtype WORD is std_logic_vector(RegWidth-1 downto 0);
 	type MEMORY is array(1 to 2**AddrBits - 1) of WORD;
 	signal registers: MEMORY := (others=> (others => '0'));
 	begin
-		ReadData1 <= (others => '0') when (ReadAddr1 = zero_reg or 
-		                ReadAddr1 = x_reg or ReadAddr1 = u_reg) else 
-						registers(to_integer(UNSIGNED(ReadAddr1)));
-        ReadData2 <= (others => '0') when (ReadAddr2 = zero_reg or 
-                        ReadAddr2 = x_reg or ReadAddr2 = u_reg) else 
-						registers(to_integer(UNSIGNED(ReadAddr2)));
-        out_value <= (others => '0') when (inr = zero_reg or
-                        inr = x_reg or inr = u_reg) else 
-						registers(to_integer(UNSIGNED(inr)));
+	    index1 <= unsigned(ReadAddr1);
+		ReadData1 <= registers(to_integer(index1)) when index1 > 0 else
+		              (others => '0');
+		index2 <= unsigned(ReadAddr2);
+        ReadData2 <= registers(to_integer(index2)) when index2 > 0 else
+                      (others => '0');
+        index3 <= unsigned(inr);
+        out_value <= registers(to_integer(index3)) when index3 > 0 else
+                      (others => '0');
 		process(CLK)
 		begin
 			if rising_edge(CLK) then
 				if RST = '1' then
 					registers <= (others => (others => '0'));
-				elsif WriteEN = '1' and not (WriteAddr = zero_reg or 
-				    WriteAddr = x_reg or WriteAddr = u_reg) then
+				elsif WriteEN = '1' and unsigned(WriteAddr) > 0 then
 					registers(to_integer(UNSIGNED(WriteAddr))) <= WriteData;
 				end if;
 			end if;
