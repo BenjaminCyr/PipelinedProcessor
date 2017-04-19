52567a749b63a402c4f5ac9b5f32e2ca737d172e
Ben Cyr <bencyr39@gmail.com>
2017-04-19 10:12:46 -0500

    Updated Control Logic to have instructions Type
    Makes it easier to see the instruction in simulator and removes a warning

--- src/CPU/ID_Stage/Control_Logic.vhd
+++ src/CPU/ID_Stage/Control_Logic.vhd
@@ -16,24 +16,28 @@ entity Control_Logic is
 end entity Control_Logic;
 
 architecture Control_Logic_Behavior of Control_Logic is
+	type instructions is (HALT, ADD, SUB, ANDD, ORR, SLT, LSI, RSI, 
+							ADDI, LW, SW, BNE, BEQ, JL, J, JR)
+	signal current_instruction : instructions := HALT;
 	begin
-		with Opcode select
-			Control_Signals <= "0000000000000" when "0000",
-							"0010100000010" when "0001",
-							"0100100000010" when "0010",
-							"0110100000010" when "0011",
-							"1000100000010" when "0100",
-							"1010100000010" when "0101",
-							"1101000000010" when "0110",
-							"1111000000010" when "0111",
-							"0011000000010" when "1000",
-							"0011000000111" when "1001",
-							"0011000001000" when "1010",
-							"0100011000000" when "1011",
-							"0100010000000" when "1100",
-							"0000000010010" when "1101",
-							"0000000000000" when "1110",
-							"0000000100000" when "1111",
+		current_instruction <= Opcode(3 downto 0);
+		with current_instruction select
+			Control_Signals <= "0000000000000" when HALT,
+							"0010100000010" when ADD,
+							"0100100000010" when SUB,
+							"0110100000010" when ANDD,
+							"1000100000010" when ORR,
+							"1010100000010" when SLT,
+							"1101000000010" when LSI,
+							"1111000000010" when RSI,
+							"0011000000010" when ADDI,
+							"0011000000111" when LW,
+							"0011000001000" when SW,
+							"0100011000000" when BNE,
+							"0100010000000" when BEQ,
+							"0000000010010" when JL,
+							"0000000000000" when J,
+							"0000000100000" when JR,
 							"XXXXXXXXXXXXX" when others;
 end architecture Control_Logic_Behavior;
 
